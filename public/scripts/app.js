1b007b24a8837ee1194bace366caa6ac5b05d290
Ashley Fernandes <ashleefernandes@gmail.com>
2018-06-27 16:51:50 +0530

    added src folder

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
+"use strict";
+
 console.log("App.js is running");
 
 //JSX
-var template = <p>This is JSX from app.js</p>
+var template = React.createElement(
+  "p",
+  null,
+  "This is JSX from app.js"
+);
 var appRoot = document.getElementById("app");
-ReactDOM.render(template,app)
+ReactDOM.render(template, app);
