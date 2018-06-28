213ff9ac35d8ae815813cca3eb559dae74ebdf40
Ashley Fernandes <ashleefernandes@gmail.com>
2018-06-28 23:45:27 +0530

    completed module 3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,98 +1,33 @@
 "use strict";
 
-console.log("This is app.js : running");
-
-var app = {
-  title: "Indecision App",
-  subtitle: "I am awesome",
-  options: []
-};
-
-var onFormSubmit = function onFormSubmit(p) {
-  p.preventDefault();
-  var option = p.target.elements.option.value;
-  if (option) {
-    app.options.push(option);
-    p.target.elements.option.value = " ";
-  }
-  console.log("form submit " + app.options);
-  renderFunction();
-};
-
+var flag = true;
 var appRoot = document.getElementById("app");
 
-var buttonReset = function buttonReset() {
-  app.options = [];
-  renderFunction();
-  console.log("button reset");
-};
-
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  alert(option);
-};
-
 var renderFunction = function renderFunction() {
-  var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "h1",
-      null,
-      app.title
-    ),
-    app.subtitle && React.createElement(
-      "p",
-      null,
-      app.subtitle
-    ),
-    React.createElement(
-      "p",
-      null,
-      app.options.length > 0 ? "here are your option" : "No Options"
-    ),
-    React.createElement(
-      "p",
-      null,
-      app.options.length
-    ),
-    React.createElement(
-      "button",
-      { disabled: app.options.length === 0, onClick: onMakeDecision },
-      "What should i do?"
-    ),
-    React.createElement(
-      "button",
-      { onClick: buttonReset },
-      "Reset"
-    ),
-    React.createElement(
-      "ol",
-      null,
-      app.options.map(function (x) {
-        return React.createElement(
-          "li",
-          { key: x },
-          x
-        );
-      })
-    ),
-    React.createElement(
-      "form",
-      { onSubmit: onFormSubmit },
-      React.createElement("input", { type: "text", name: "option" }),
-      React.createElement(
-        "button",
-        null,
-        "Add Option"
-      )
-    )
-  );
+      var header = React.createElement(
+            "div",
+            null,
+            React.createElement(
+                  "h1",
+                  null,
+                  "Visibility Toggle"
+            ),
+            React.createElement(
+                  "button",
+                  { onClick: toggle },
+                  flag ? "Show text" : "Hide text"
+            ),
+            React.createElement(
+                  "p",
+                  { hidden: flag },
+                  "This is a piece of text"
+            )
+      );
 
-  ReactDOM.render(template, appRoot);
+      ReactDOM.render(header, appRoot);
 };
-//JSX
-
-
+var toggle = function toggle() {
+      flag = !flag;
+      renderFunction();
+};
 renderFunction();
---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,19 @@
+let flag = true ;
+const appRoot = document.getElementById("app")
+
+const renderFunction = () => {
+  const header =
+              <div>
+              <h1>Visibility Toggle</h1>
+              <button onClick={toggle}>{flag?"Show text":"Hide text"}</button>
+              <p hidden={flag}>This is a piece of text</p>
+              </div>
+
+              ReactDOM.render(header,appRoot)
+
+};
+const toggle = () =>{
+      flag = !flag;
+      renderFunction();
+}
+renderFunction();
