b41fc621935de305ab4fc8410b32c5cc091f61a0
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-07 19:34:34 +0530

    vid 060

--- src/app.js
+++ src/app.js
@@ -2,31 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
+const Layout = props => {
+  return (
+    <div>
+      <p>header</p>
+      {props.children}
+      <p>footer</p>
+    </div>
+  );
+};
+
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
 
-class OldSyntax {
-  constructor() {
-    this.name = "Mike";
-    this.getGreeting = this.getGreeting.bind(this);
-  }
-  getGreeting() {
-    return `HI my name is 1  ${this.name}`;
-  }
-}
-const oldSyntax = new OldSyntax();
-const gg = oldSyntax.getGreeting;
-console.log(gg());
-
 //
+// class OldSyntax {
+//   constructor() {
+//     this.name = "Mike";
+//     this.getGreeting = this.getGreeting.bind(this);
+//   }
+//   getGreeting() {
+//     return `HI my name is 1  ${this.name}`;
+//   }
+// }
+// const oldSyntax = new OldSyntax();
+// const gg = oldSyntax.getGreeting;
+// console.log(gg());
 //
-//
-//
-class NewSyntax {
-  name = "Jen";
-  getGreeting = () => {
-    return `HI my name is 1  ${this.name}`;
-  };
-}
-const newSyntax = new NewSyntax();
-const ff = newSyntax.getGreeting;
-console.log(ff());
+// //
+// //
+// //
+// //
+// class NewSyntax {
+//   name = "Jen";
+//   getGreeting = () => {
+//     return `HI my name is 1  ${this.name}`;
+//   };
+// }
+// const newSyntax = new NewSyntax();
+// const ff = newSyntax.getGreeting;
+// console.log(ff());
