ca0566618f857f58958f608abd2205817e351f70
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-07 23:40:52 +0530

    password test

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 import "./styles/styles.scss";
 
+console.log("yo");
+
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
 
 //
