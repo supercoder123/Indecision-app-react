83a8c5d10a673a45eaffa9c9ef53da00682fa52f
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-09 01:07:21 +0530

    finished styling indecision app

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,16 @@ const OptionModal = props => (
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
     onRequestClose={props.closeModal}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.closeModal}>Ok</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && (
+      <p className="modal__body">{props.selectedOption}</p>
+    )}
+    <button className="button" onClick={props.closeModal}>
+      Ok
+    </button>
   </Modal>
 );
 
